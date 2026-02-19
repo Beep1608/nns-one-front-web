482fc02f37665d63f2b7c64f9c389bac356d071c
Kevin López González <nightmare28899@Kevins-MacBook-Pro.local>
2026-02-19 12:25:19 -0600

    change to the routes it cause a bug on the deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,31 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'products',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'products/create',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'users',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'users/create',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'users/edit/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Client,
+  },
 ];
